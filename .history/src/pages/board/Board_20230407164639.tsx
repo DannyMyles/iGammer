6103537a2332be72383ffd5dc7e2d6d630d395c4
DannyMyles <dannymyles1999@gmail.com>
2023-04-08 09:46:51 +0300

    Adds game logic

---
+++ .history/src/pages/board/Board_20230407164639.tsx
@@ -0,0 +1,59 @@
+
+import React, { useEffect, useState } from "react";
+import { BoardProps, TitleState } from "../../models/games.models";
+import { gameSolution, initBoard } from "../../services/game-logic";
+
+
+
+const Board = ({ tilesPerRow, width, imageUrl }: BoardProps) => {
+  
+  const [tiles, setTiles] = useState<TitleState[]>(initBoard(
+    gameSolution(tilesPerRow, width),
+    tilesPerRow,
+    width,
+    imageUrl
+  ));
+
+  // Initialize the board
+  
+
+  useEffect(() => {
+
+    // console.log(gameSolution())
+
+    // const totalTiles:Number = (tilesPerRow || 3)**2;
+    // setTotalTiles(totalTiles);
+    // SetTileWidth(width / tilesPerRow);
+    // initBoard();
+    // setSolution(gameSolution());
+    // console.table(solution);
+    // setCurrentTileState(shuffleBoard())
+    
+    // console.table(currentTileState);
+    
+    // setTiles(initBoard(solution)); 
+
+    // setTimeout(shuffleBoard, 5000);
+  }, []);
+
+  return (
+    <div
+      style={{
+        position: "absolute",
+        background: `rgb(236, 236, 236) url(${imageUrl})`,
+        backgroundSize: "",
+        width: `${(width / tilesPerRow) * tilesPerRow}px`,
+        height: `${(width / tilesPerRow) * tilesPerRow}px`,
+      }}
+    >
+      {tiles.map((tile, i) => (
+        <div key={i + 1} id={`tile-{i}`} style={tile}>
+          {i}
+        </div>
+
+      ))}
+    </div>
+  );
+};
+
+export default Board;
---
+++ .history/src/pages/register/Register_20230406113857.tsx
@@ -0,0 +1,98 @@
+import "./register.css";
+import { useState } from "react";
+import Footer from "../../components/footer/Footer";
+import Navbar from "../../components/navbar/Navbar";
+
+type FormData = {
+  firstName: string,
+    lastName: string,
+    email: string,
+    password: string,
+    birthDate: string
+}
+
+
+const initialFormData: FormData = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  birthDate: ""
+};
+const Register = () => {
+  const [formData, setFormData] = useState<FormData>(initialFormData);
+
+  const handleChange =  (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+
+  // const handleRegistration = (e: React.FormEvent<HTMLFormElement>) => {
+  //   e.preventDefault();
+  //   onSubmit(firstName, lastName, email, password,birthDate);
+  // };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // Your form submission logic goes here
+    // ...
+
+    // Reset form input values to their initial state
+    setFormData(initialFormData);
+  };
+  return (
+    <>
+    <Navbar />
+    <div className="register-container">
+      <form className="registration-form" onSubmit={handleSubmit}>
+        <h2>Register</h2>
+        <label>
+          First Name:
+          <input
+            type="text"
+            value={formData.firstName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Last Name:
+          <input
+            type="text"
+            value={formData.lastName}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Birth Date:
+          <input
+            type="date"
+            value={formData.birthDate}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Email:
+          <input
+            type="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password:
+          <input
+            type="password"
+            value={formData.password}
+            onChange={handleChange}
+          />
+        </label>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+    <Footer />
+    </>
+    
+  );
+};
+
+export default Register;
